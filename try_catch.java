6372536147e395dd655da15a813c495c88130025
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-07 22:33:58 +0300

    .

--- try_catch.java
+++ try_catch.java
@@ -32,3 +32,7 @@ public class try_catch
 //? Kuzu try kısmına yazdığımız yerde hata bulduğu zaman catch kısmını konntrol ediyor eğer catch kısmında biz bu hatdan bahsetmişsek
 //? uygun catch bloğunun içine giriyor ve kodu çalıştırıyor bu da bittikten sonra artık try bloğuna da diğer catchlere de devam etmiyor
 //? alta geçip oradaki kodlardan devam ediyor. Eğer try catch olmasaydı hatadan sonra devam etmez programı derlemeyi bırakırdı.
+//? pekii ya try kısmını içind ebulunan hatayı cath'e yazmamışask orada bulamazsa işte o zmn hoppala ne namladım bn ne anladım bu isten
+//? der ve hatayı verip derlemeyi bırakır çünkü catch kısımını kullanamyınca try 'da hatayı buklsa ne olacak yapacak ek bir şey yok.
+//? işte normalde ne olur hata bukunur hatayı ekrana basar ve derlemeyi bırakır. Java ise bize bir şans daha veriyor try bloğunun içine yazdığın
+//? kısımda hata bulursam bu hata için olan catch'teki kodu çalıştırır try'dan çıkıp !koda devamm ederim! diyor :)) 
