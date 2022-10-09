aaa35838cd26fd11318a384f7d656499d7d05076
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-09 21:15:22 +0300

    .

---
+++ Week4_ex6/Stati2_test.java
@@ -0,0 +1,20 @@
+package Week4_ex6;
+
+public class Stati2_test 
+{
+    public static void main(String[] Soylu)
+    {
+        Static2_ s1=new Static2_(1,"Sabriye",10);
+        Static2_ s2=new Static2_(2,"Zeynep",20);
+        Static2_ s3=new Static2_(3,"NEcmiye",30);
+        s1.print_info();
+        s2.print_info();
+        s3.print_info();
+
+        s1.set("MNG");//! AAAA statiği değiştirebiliyoruz. 
+                            //? Dikkat etmemiz gereken statşc bir tane olduğu için değiştiğinde tüm objeler için değişir.
+        s1.print_info();
+        s2.print_info();
+        s3.print_info();
+    }    
+}
