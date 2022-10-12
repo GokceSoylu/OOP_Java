1945fa8c431fd7dbc4de544e0e13564c22327429
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-12 21:22:09 +0300

    .

---
+++ StaticMethod0.java
@@ -0,0 +1,23 @@
+public class StaticMethod0 
+{
+    public static void main(String[] Soylu)
+    {
+        StaticMethod0 m=new StaticMethod0();
+        m.print();//objesiz çağırılamadı
+        
+        print_s();//direkt çağırdık :))
+    }
+    static void print_s()
+    {
+        System.out.println("hello from static :))");
+    }
+    void print()
+    {
+        System.out.println("hello from non-static");
+    }
+}
+//Static varible'in ne olduğunu biliyoruz.
+//-her obje bellekte yaşayan ayrı hücreler gibiydi bizde her biri için aynı olan vb durunlar için
+//bellekte tek bir yer kaplayacak tek bir olan tüm objelerin kullanacaği bir varaible yapalım dedeik. Bize bunu sağlayan static keyword oluyor
+//varaible durum buydu metodda da aynı mantık var. her obje için ayrı ayrı olmayan tüm objelerin kullancağı, nesne olmadan çağırılabilecek metodlar
+//oluşturmak isteğimizde static keyword'ü kullanıyoruz. 
