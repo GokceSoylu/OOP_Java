5dd2811c18e051f061db1a049df991ae54ed9988
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-25 19:38:22 +0300

    .

--- Try_catch/Try_catch10.java
+++ Try_catch/Try_catch10.java
@@ -4,10 +4,9 @@ public class Try_catch10
 {
     public static void main(String[] Soylu)
     {
-        float c=2, b=1, d;
-        d=89;
-        System.out.println("c = "+c+" b = "+b+" d = "+d);
-        int a;
+        float c=2, b=1, d=89.2f;
+        System.out.println("c = "+c+" b = "+b+" d = "+d);//? arada floattan da bahsedelim eğer noktasız direkt int gibi sayı atayacaksak sorun yokmancak 
+        int a;                                             //? ancak noktalı atayacaksak bunu double kabul eder ve haat veriri float olduğunu belirtmek için sonuna f yazarız :))
         try
         {
             Scanner in=new Scanner(System.in);
