cc05355907d2836d4271466bab25b3cb9929fa20
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-10-30 23:46:47 +0300

    .

---
+++ Nasted_clases/innerClases/Private_constructer.java
@@ -0,0 +1,21 @@
+package Nasted_clases.innerClases;
+
+public class Private_constructer 
+{
+    String name;
+    public void the_inner(int data)
+    {
+        Inner i=new Inner(data);
+    }
+    class Inner
+    {
+        private int data;
+        private int counter;
+        private Inner(int data)
+        {
+            this.data=data;
+        }
+    }
+    
+
+}
