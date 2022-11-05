95d210d5b76aa1789bcf0da276b978eba0416ac7
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-11-05 18:12:41 +0300

    .

---
+++ Enum3.java
@@ -0,0 +1,27 @@
+public class Enum3 
+{
+    enum Lectures
+    {
+        oop(5), data(5), logic(-2), circuit(-1), dif(0);//sınıf içerisinde değil direkt oluşturalim
+        private int like;
+        Lectures(int like)
+        {
+            this.like=like;
+        }
+    }    
+    public static void main(String[] Soylu)
+    {
+        Lectures dis=Lectures.logic;
+        Lectures like=Lectures.data;
+        System.out.println(dis);
+        System.out.printf("\nlike = %d",like.like);
+
+    }
+}
+//AA enum kesinlikle c'deki gibi değil C'de sadece int değer atayabiliyorduk eğer ataamzsak kendisi birden başlayıp sırayla birir artırark
+//değer atıyordu. Burada ise bildiğin sınıf gibi davranıyor.
+//eveet sınıf GİBİ davranıyor ancak sınıf değil farkları nelerdir?
+//her zaman public'tir, sabittir(const like final), extends edilemez(inheritance yapılamaz (kalıtım)) ve obje üretemez.
+//ow ancak yukarıda yaptığımızda nedir? bildiğin gibi c++'da obje üretirken direkt varaible oluşturur gibi oluşturabilirdik. ancak Java'da
+//durum farklı obje için her zaman new keyword'ünü kallanmalıyız. kullamadan oluşturduğumuz şey obje değil obje refaransı olur. bu uzun açıklamadan sonra 
+//yukarıda enum'dan refarance bir nevi varaible oluşturduk obje değil. Enum obje oluşturamaz :))
