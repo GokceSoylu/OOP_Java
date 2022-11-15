9cf7e48a41229ae615dd1d20551cddfdefc686ca
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-11-15 12:58:28 +0300

    .

--- Creat_obje/ConstructerFromContructer.java
+++ Creat_obje/ConstructerFromContructer.java
@@ -5,11 +5,7 @@ public class ConstructerFromContructer
     {    
         Person_ p1=new Person_("hasn", "Can", 12, 01), p2=new Person_("İlber", "Ortayli");
         Person_ p3=new Person_(12), p4=new Person_(); //normalde üçüncü ve dördüncü contructerlar diğer fonklar çağirsin diye de biz yinede deneme amaçlı obje oluştorduk
-        Person_[] ar = new Person_[4];
-        ar[0]=p1;
-        ar[1]=p2;
-        ar[2]=p3;
-        ar[3]=p4;
+        Person_[] ar = new Person_[]{p1,p2,p3,p4};
         for(Person_ p:ar)
             System.out.println(p.firstName+" "+p.lastName+" "+p.age+" "+p.id);
     }    
--- Creat_obje/inMethodParanthes.java
+++ Creat_obje/inMethodParanthes.java
@@ -2,12 +2,15 @@ package Creat_obje;
 
 public class inMethodParanthes 
 {
-    Human h1=new Human("e");
-    Human h2=new Human("w");
-    Human h3 = new Human("s");
-    Human[]h=new Human[3];//Human(h1,h2,h3);
-    Sinif(new Human("t"));
-
+    public static void main(String[] Soylu)
+    {
+        Human h1=new Human("e");
+        Human h2=new Human("w");
+        Human h3 = new Human("s");
+        Human[]h=new Human[]{h1,h2,h3,new Human("q")};//! Önemli
+        Sinif s=new Sinif(h);
+        s.printStd();   
+    }
 }
 class Human
 {
@@ -19,9 +22,15 @@ class Human
 }
 class Sinif
 {
-    Human human;
-    Sinif(Human human)
+    Human[] students;
+    Sinif(Human[] human)
     {
-        this.human=human;
+        students=human;
+    }
+    void printStd()
+    {
+        int i=1;
+        for(Human h:students)
+            System.out.println((i++) +" "+h.name);
     }
 }
