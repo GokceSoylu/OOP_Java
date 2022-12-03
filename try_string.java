6043b46bdbbc99a18c14e5f0daf98974f9e0df77
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-03 22:25:26 +0300

    string aritmatic

---
+++ try_string.java
@@ -0,0 +1,11 @@
+public class try_string 
+{
+    public static void main(String[] Soylu)
+    {
+        String a="hasan celebi ";
+        String b="urfa da";
+        String c=a+b;
+        System.out.println(c);
+    }    
+}
+
