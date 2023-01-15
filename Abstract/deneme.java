ba12991f35e545ad60222d75810ea6ec783dcfde
NecmiyeSoylu <necmiyesoyla@gmail.com>
2023-01-15 23:02:38 +0300

    flaot bölme

---
+++ Abstract/deneme.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+public class deneme {
+    
+    public static void main(String[] Soylu)
+    {
+        int a=10, b=3;
+        System.out.println(f(a,b));
+    }
+    static double f(int a, int b)
+        {
+            return (double)a/b;
+        }
+}
